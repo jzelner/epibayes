2a98459b75e3bff1a6be98855fe68edb212e6fad
Jon Zelner <jzelner@gmail.com>
2012-11-26 12:42:28 -0500

    sync

--- patch/patch.go
+++ patch/patch.go
@@ -1,6 +1,7 @@
 package patch
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 )
@@ -22,9 +23,92 @@ func (bi *ConstantRate) At(t int) float64 {
 	return bi.Alpha
 }
 
+type DecayingInfectionSource struct {
+	//This is the overall rate of transmission,
+	//i.e. when there is no distance from the source
+	alpha float64
+
+	//This is the decay rate per unit of distance
+	//or remoteness
+	phi float64
+
+	distance float64
+	popSize  float64
+
+	//Because this is constant, we'll cache the value when 
+	//it's set instead of calling over and over
+	cachedRate float64
+}
+
+func (d *DecayingInfectionSource) PopSize() float64 { return d.popSize }
+func (d *DecayingInfectionSource) SetPopSize(px float64) {
+	d.popSize = px
+	d.calculateRate()
+}
+
+func (d *DecayingInfectionSource) Distance() float64 { return d.distance }
+func (d *DecayingInfectionSource) SetDistance(dx float64) {
+	d.distance = dx
+	d.calculateRate()
+}
+
+func (d *DecayingInfectionSource) Alpha() float64 { return d.alpha }
+func (d *DecayingInfectionSource) SetAlpha(ax float64) {
+	d.alpha = ax
+	d.calculateRate()
+}
+
+func (d *DecayingInfectionSource) Phi() float64 { return d.phi }
+func (d *DecayingInfectionSource) SetPhi(px float64) {
+	d.phi = px
+	d.calculateRate()
+}
+
+func (d *DecayingInfectionSource) calculateRate() {
+	d.cachedRate = d.alpha * d.popSize * math.Exp(-d.phi*d.distance)
+}
+func (d *DecayingInfectionSource) At(x int) float64 {
+	return d.cachedRate
+}
+
+func (d *DecayingInfectionSource) GobEncode() ([]byte, error) {
+	vals := map[string]float64{"alpha": d.alpha, "popSize": d.popSize, "distance": d.distance, "phi": d.phi}
+	b, err := json.Marshal(vals)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func (d *DecayingInfectionSource) GobDecode(b []byte) error {
+	vals := map[string]float64{}
+	err := json.Unmarshal(b, &vals)
+	if err != nil {
+		return err
+	}
+
+	d.alpha = vals["alpha"]
+	d.phi = vals["phi"]
+	d.popSize = vals["popSize"]
+	d.distance = vals["distance"]
+
+	d.calculateRate()
+	return nil
+}
+func NewDecayingInfectionSource(alpha, phi, distance, popSize float64) *DecayingInfectionSource {
+	d := &DecayingInfectionSource{alpha, phi, distance, popSize, 0.0}
+	d.calculateRate()
+	return d
+}
+
 type InfectionSource interface {
 	At(int) float64
 }
+
+//SIRModel has an embedded network
+//in it, as well as a counter, T, 
+//for the current time
 type SIRModel struct {
 	*Network
 	T int
@@ -43,6 +127,7 @@ func (m *SIRModel) SetGravityWeights(p Parameters) {
 
 func (m *SIRModel) Initialize(p Parameters) float64 {
 	totalI := 0.0
+	m.T = 0
 	m.SetGravityWeights(p)
 	for _, n := range m.Nodes {
 		totalI += n.P.Initialize(p.Gamma)
@@ -52,6 +137,8 @@ func (m *SIRModel) Initialize(p Parameters) float64 {
 
 func (m *SIRModel) Step(p Parameters, externalInf map[string]InfectionSource) float64 {
 	totalI := 0.0
+	//Loop through the nodes 
+	//in the spatial network
 	for k, n := range m.Nodes {
 		var ei float64
 		s, ok := externalInf[k]
@@ -120,6 +207,12 @@ func (se *SpatialEdge) String() string {
 	return fmt.Sprintf("%s -> %s; Dist : %0.2f, Weight :%0.2g", se.From.ID, se.To.ID, se.Distance, se.Weight)
 }
 
+//Network implements a simple spatial network
+//where interactions between nodes are governed
+//by the weights of spatial edges. The weights
+//indicate the impact of prevalence in the
+//sending community on risk in the receiving
+//community.
 type Network struct {
 	Edges         []*SpatialEdge
 	Nodes         map[string]*PatchNode
@@ -136,6 +229,9 @@ func (n *Network) AddPatch(id string, p Patch) *PatchNode {
 }
 
 func (n *Network) AddEdge(from, to string, distance float64) *SpatialEdge {
+	if from == to {
+		return nil
+	}
 	e := new(SpatialEdge)
 	e.Weight = 1.0
 	e.Distance = distance
