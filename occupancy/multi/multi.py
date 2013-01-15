2ca359746590218e897656225aca6af807465d8c
Jon Zelner <jzelner@gmail.com>
2013-01-15 15:06:22 -0500

    Changes to multipathogen code

--- occupancy/multi/multi.py
+++ occupancy/multi/multi.py
@@ -2,6 +2,9 @@ import numpy as np
 import pymc
 import time
 
+#generates a list of matrices of length n_locations,
+#where each list element is a matrix with n_pathogens rows
+#and n_steps columns
 def randomPathogens(n_pathogens, n_steps, n_loc, p):
 	pathogen_presence = [np.reshape(np.random.binomial(1, p, n_pathogens*n_steps), (n_pathogens, n_steps)) for i in xrange(n_locations)]	
 	return pathogen_presence
@@ -27,17 +30,18 @@ def pairwiseExposure(pathogens, weights):
 				continue
 			exposures[j] = exposures[j] + w*p
 
-	return exposures
+	return np.array(exposures)
 
 def PairwiseExposure(name, pathogens, weights):
 	pe = pymc.Deterministic(name = name, doc = "Calculates pairwise exposures between sets of locations.", eval = pairwiseExposure, parents = {"pathogens" : pathogens, "weights" : weights})
 	return pe
 
 def separatePastAndFutureState(state):
-	pastStates = [s[:,0:s.shape[1]-1] for s in state]
-	futureStates = [s[:,1:s.shape[1]] for s in state]
-	ic = [s[:,0] for s in state]
-	return(ic, pastStates, futureStates)
+	print(state)
+	pastStates = np.array([s[:,0:s.shape[1]-1] for s in state])
+	futureStates = np.array([s[:,1:s.shape[1]] for s in state])
+	ic = np.array([s[:,0] for s in state])
+	return {"IC":ic, "PS":pastStates, "FS":futureStates}
 
 def SeparatePastAndFutureState(name, state):
 	s = pymc.Deterministic(name = name, doc = "Extracts initial conditions, lagged states and future states", eval = separatePastAndFutureState, parents = {"state" : state})
@@ -47,30 +51,42 @@ def withinLocationLag(pathogens, autocorr):
 	lag = [np.zeros_like(p) for p in pathogens]
 	for i, a in enumerate(autocorr):
 		lag[i] = lag[i] + a*pathogens[i]
-	return lag
+	return np.array(lag)
 
 def WithinLocationLag(name, pathogens, autocorr):
 	wl = pymc.Deterministic(name = name, doc = "Gets logit for lag within a location", eval = withinLocationLag, parents = {"pathogens":pathogens, "autocorr":autocorr})
 	return wl
 
 def independentPredictor(pathogens, predictors):
-	ip = [pr*np.ones_like(pathogens[i]) for i,pr in enumerate(predictors)]
+	ip = np.array([pr*np.ones_like(pathogens[i]) for i,pr in enumerate(predictors)])
 	return ip
 
 def IndependentPredictor(name, pathogens, predictors):
 	ip = pymc.Deterministic(name = name, doc = "Independent predictors", eval = independentPredictor, parents = {"pathogens":pathogens, "predictors": predictors})
 	return ip
 
-def predictedStateProbability(value, ex, ac, iv):
+def predictedStateProbability(value, ex, ac, iv, fs):
 	total_prob = []
 	tp = 0.0
-	for i,x in enumerate(value):
+	for i,x in enumerate(fs):
 		tp += pymc.bernoulli_like(x, pymc.invlogit(ex[i] + ac[i] + iv[i]))
 	return tp
 
-def PredictedStateProbability(name, ex, ac, iv, value = None):
-	ps = pymc.Stochastic(name = name, doc = "Probability of next state", logp = predictedStateProbability, parents = {"ex" : ex, "ac":ac, "iv":iv}, value = value)
+#ex = external exposure, ac = within-location autocorrelation,
+#iv = independent variables
+def PredictedStateProbability(name, ex, ac, iv, fs, value = 0.0):
+	ps = pymc.Stochastic(name = name, doc = "Probability of next state", logp = predictedStateProbability, parents = {"ex" : ex, "ac":ac, "iv":iv, "fs" : fs}, value = value, dtype = float, observed = True)
 	return ps
+
+def cs_logp(value):
+	return 0.0
+
+def CompleteState(name, s):
+	cs = pymc.Stochastic(name = name, doc = "Placeholder for total system state that facilitates manipulation by mcmc.", trace = False, cache_depth = 2, parents = {}, value = s, logp = cs_logp)
+	return cs
+
+#Want to manipulate the underlying state in a way that doesn't conflict with 
+#the observations
 
 if __name__ == '__main__':
 	#Make a random matrix representing pathogens over time in each village
@@ -79,10 +95,12 @@ if __name__ == '__main__':
 	n_locations = 25
 
 	r_path = randomPathogens(n_pathogens, n_times, n_locations, 0.5)
-	w = randomWeights(n_locations)
-	s = SeparatePastAndFutureState("Separator", r_path)
 
-	initial_conditions, last_path, next_path = (s.value[0], s.value[1], s.value[2])
+	cs = CompleteState("CS", r_path)
+	w = randomWeights(n_locations)
+	s = SeparatePastAndFutureState("Separator", cs)
+
+	initial_conditions, last_path, next_path = (s.value["IC"], s.value["PS"], s.value["FS"])
 
 	pe = PairwiseExposure("Pairwise exposure", last_path, w)
 	wl = WithinLocationLag("Lag", last_path, np.random.uniform(size = n_locations))
@@ -90,3 +108,5 @@ if __name__ == '__main__':
 
 	ps = PredictedStateProbability("MANZE", ip, wl, pe, value = next_path)
 	print(ps.logp)
+
+	print(s["IC"].value)
