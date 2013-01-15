e9578abb58552f44bf5c108c115921c976a50d31
Jon Zelner <jzelner@gmail.com>
2013-01-15 16:03:58 -0500

    sync

--- occupancy/multi/multi.py
+++ occupancy/multi/multi.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pymc
 import time
+import random
 
 #generates a list of matrices of length n_locations,
 #where each list element is a matrix with n_pathogens rows
@@ -37,7 +38,6 @@ def PairwiseExposure(name, pathogens, weights):
 	return pe
 
 def separatePastAndFutureState(state):
-	print(state)
 	pastStates = np.array([s[:,0:s.shape[1]-1] for s in state])
 	futureStates = np.array([s[:,1:s.shape[1]] for s in state])
 	ic = np.array([s[:,0] for s in state])
@@ -84,6 +84,35 @@ def cs_logp(value):
 def CompleteState(name, s):
 	cs = pymc.Stochastic(name = name, doc = "Placeholder for total system state that facilitates manipulation by mcmc.", trace = False, cache_depth = 2, parents = {}, value = s, logp = cs_logp)
 	return cs
+
+
+class StateMetropolis(pymc.Metropolis):
+
+	def __init__(self, stochastic, sample_p, unobserved_indices, *args, **kwargs):
+		self.sample_p = sample_p
+		self.unobserved_indices = unobserved_indices
+		self.num_changeable = len(self.unobserved_indices[0])
+		self.last_value = np.zeros_like(self.unobserved_indices[0])
+		pymc.Metropolis.__init__(self, stochastic, *args, **kwargs)
+
+	def propose(self):
+		p = min(max(0.01, self.adaptive_scale_factor*self.sample_p), 0.99)
+		
+		num_to_sample = max(1, np.random.binomial(self.num_changeable, p))
+
+		self.last_sampled_indices = zip(*random.sample(self.unobserved_indices, num_to_sample))
+
+		self.last_value = self.stochastic.value
+		val = np.copy(self.stochastic.value)
+		val[self.last_sampled_indices] = 1.0 - self.last_value[self.last_sampled_indices]
+		self.stochastic.value = val
+
+	def reject(self):
+		self.stochastic.value = self.last_value
+
+	def hastings_factor(self):
+		return 0.0
+
 
 #Want to manipulate the underlying state in a way that doesn't conflict with 
 #the observations
